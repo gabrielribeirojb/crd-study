5faef4e8d28e6ab4f215325436d86c8f23935692
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2026-03-09 11:08:33 -0300

    Ajustando código

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -6,9 +6,11 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 CODEGEN_PKG="${ROOT_DIR}/vendor/k8s.io/code-generator"
 
-bash "${CODEGEN_PKG}/generate-groups.sh" "deepcopy" \
-  "${MODULE}/pkg/apis" \
-  "${MODULE}/pkg/apis" \
-  "gdch:v1" \
+bash "${CODEGEN_PKG}/kube_codegen.sh" \
   --output-base "${ROOT_DIR}" \
-  --go-header-file "${ROOT_DIR}/hack/boilerplate.go.txt"
+  --go-header-file "${ROOT_DIR}/hack/boilerplate.go.txt" \
+  --boilerplate "${ROOT_DIR}/hack/boilerplate.go.txt" \
+  --modules "${MODULE}" \
+  --apis-dir "${MODULE}/pkg/apis" \
+  --groups "gdch:v1" \
+  --generate "deepcopy,client,informer,lister"
